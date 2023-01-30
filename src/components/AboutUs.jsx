bb0f37a17200b4999da8bf7202521e764d0c4223
alekosomegas <kangkelidis@gmail.com>
2023-01-30 10:50:34 +0000

    first deploy

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,8 +3,9 @@ export default function AboutUs() {
         <div id="AboutUs">
             <div className='divider'>
         </div>
+        <h3> Allure Taxi Limassol, Cyprus</h3>
         <div className="info">
-            <p>Email: info@allure.com</p>
+            <p>Email: kangkelidis@gmail.com</p>
             <p>Tel: +35799667777</p>
             <p>Address: Zana Complex, Amathus Avenue, 4061 </p>
         </div>
